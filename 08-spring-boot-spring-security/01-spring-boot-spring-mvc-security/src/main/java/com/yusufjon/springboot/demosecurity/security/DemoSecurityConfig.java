194d2b70736d3cfaae5dced6c06504b627513338
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-04-19 15:31:47 +0500

    restrict URL

--- 08-spring-boot-spring-security/01-spring-boot-spring-mvc-security/src/main/java/com/yusufjon/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-security/01-spring-boot-spring-mvc-security/src/main/java/com/yusufjon/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -41,6 +41,9 @@ public class DemoSecurityConfig {
 
         http.authorizeHttpRequests(configuser ->
                         configuser
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .formLogin(form ->
