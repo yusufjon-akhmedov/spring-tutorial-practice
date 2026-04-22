61ddaf4b02cedb5eb9537a51b71c0778f2886b4c
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-04-22 11:31:33 +0500

    aop

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/yusufjon/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/yusufjon/aopdemo/AopdemoApplication.java
@@ -1,13 +1,36 @@
 package com.yusufjon.aopdemo;
 
+import com.yusufjon.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+
+		return runner -> {
+
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+
+		theAccountDAO.addAccount();
+
+
+		System.out.println("\n let's call it again!\n");
+
+		theAccountDAO.addAccount();
 	}
 
 }
---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/yusufjon/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,16 @@
+package com.yusufjon.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+    @Before("execution(public void addAccount())")
+    public void beforeAddAccountAdvice() {
+
+        System.out.println("\n=====>>> Executing @Before on addAccount()");
+    }
+}
